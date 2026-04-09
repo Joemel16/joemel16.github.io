96b3b682003682f3c7fc561a288e06b4f6bae946
Joemel16 <118241096+Joemel16@users.noreply.github.com>
2026-04-09 13:26:32 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+/* =========================== typing animen text===================*/
+
+var typed = new  Typed(".typing,", {
+    strings:["","IT Specialist"," Network Administrator"],
+    typeSpeed:100,
+    BackSpeed:600,
+    loop:true
+})
